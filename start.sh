65141338e6b562318c36cd83e191270c6823f861
Marie-Laure Creno <crenomarie@gmail.com>
2023-01-15 14:57:50 +0100

    [CHR] Add dev packaging (Docker)

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Starting my-cellar project..."
+
+docker-compose up -d
+
+echo "my-cellar started !"
---
+++ stop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Stopping my-cellar project..."
+
+docker-compose down
+
+echo "my-cellar stopped !"
